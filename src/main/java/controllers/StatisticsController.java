343ad4d5c3d0971f3bd2907a378155a19d9cb694
ningge <weiguangningtian@gmail.com>
2017-12-19 00:08:25 +0800

    add week data interface

--- src/main/java/controllers/StatisticsController.java
+++ src/main/java/controllers/StatisticsController.java
@@ -13,23 +13,58 @@ import spark.Request;
 import spark.Response;
 
 import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class StatisticsController {
 
     public static String weekData(Request request, Response response) {
         try {
-            List<String[]> result = ReservationController.reservationDao.queryRaw("select * from orders  where DATE_SUB(CURDATE(), INTERVAL 7 DAY) <= date(add_time)").getResults();
+            List<String[]> result = ReservationController.reservationDao.queryRaw("select * from orders  where DATE_SUB(CURDATE(), INTERVAL 6 DAY) <= date(add_time)").getResults();
 
-            return Utils.response(true, "", result);
+            int[] finalResult = new int[7];
+
+            String [] weeks = StatisticsController.getWeekArray();
+            for (String [] temp : result) {
+                int week = StatisticsController.getWeek(weeks, temp[5]);
+                finalResult[week]++;
+
+            }
+
+            return Utils.response(true, "", finalResult);
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
 
         return Utils.response(false, "查询失败", null);
+    }
+
+    private static String[] getWeekArray() {
+        Date now = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        java.util.Calendar cal = java.util.Calendar.getInstance();
+        cal.setTime(now);
+        cal.add(java.util.Calendar.DATE, - 7); // 向前一周；如果需要向后一周，用正数即可
+        String[] result = new String[7];
+
+
+        for(int i = 1; i <= 7; i++) {
+            cal.add(Calendar.DATE, 1);
+            result[i - 1] = format.format(cal.getTime());
+        }
+
+        return result;
+    }
+
+    private static int getWeek(String [] weeks, String date) {
+        for(int i = 0; i < weeks.length; i++) {
+            if(date.indexOf(weeks[i]) >= 0) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
